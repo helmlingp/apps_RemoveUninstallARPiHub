d5a9e28845bb1fd2beb3c15dcfc59fe0eaaa1cbe
helmlingp <66004946+helmlingp@users.noreply.github.com>
2020-08-24 10:37:38 +0930

    Create RemoveUninstallARP_v1.ps1

---
+++ RemoveUninstallARP_v1.ps1
@@ -0,0 +1,14 @@
+﻿$reg = Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" 
+
+foreach($r in $reg) {
+    if(($r.Publisher -like "*VMware*") -and ($r.DisplayName -like "*Workspace One*")){
+    $psChildName = $r.PSChildName
+    $regPath = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$psChildName"
+        if($r.NoRemove -eq $null) {
+            New-ItemProperty -Path $regPath -Name "NoRemove" -PropertyType DWORD -Value 1
+        }
+        else {
+            Set-ItemProperty -Path $regPath -Name "NoRemove" -Value 1
+        }
+    }
+}
